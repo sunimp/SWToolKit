7521292481dd719e09399fa9e153bb9b75e243d9
ant013 <ant013@mail.ru>
2022-01-20 12:54:03 +0600

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "HsToolKit",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "HsToolKit",
+            targets: ["HsToolKit"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.0")),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "4.1.0")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.1")),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.0.0")),
+    ],
+    targets: [
+        .target(
+            name: "HsToolKit",
+            dependencies: ["Alamofire", "ObjectMapper", "RxSwift", .product(name: "RxCocoa", package: "RxSwift"), .product(name: "NIOSSL", package: "swift-nio-ssl"), .product(name: "NIOWebSocket", package: "swift-nio"), .product(name: "NIOFoundationCompat", package: "swift-nio")]),
+    ]
+)
