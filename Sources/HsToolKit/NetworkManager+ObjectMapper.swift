e03757126345f12fbfd9703ec8455224f34dbb4e
Ermat <ealymbaev@gmail.com>
2023-03-15 18:08:47 +0600

    Add ability to provide MapContext to ObjectMapper

--- Sources/HsToolKit/NetworkManager+ObjectMapper.swift
+++ Sources/HsToolKit/NetworkManager+ObjectMapper.swift
@@ -5,24 +5,24 @@ import RxSwift
 
 extension NetworkManager {
 
-    public func single<T: ImmutableMappable>(request: DataRequest, sync: Bool = false, postDelay: TimeInterval? = nil) -> Single<T> {
-        single(request: request, mapper: ObjectMapper<T>(), sync: sync, postDelay: postDelay)
+    public func single<T: ImmutableMappable>(request: DataRequest, sync: Bool = false, postDelay: TimeInterval? = nil, context: MapContext? = nil) -> Single<T> {
+        single(request: request, mapper: ObjectMapper<T>(context: context), sync: sync, postDelay: postDelay)
     }
 
-    public func single<T: ImmutableMappable>(request: DataRequest) -> Single<[T]> {
-        single(request: request, mapper: ObjectArrayMapper<T>())
+    public func single<T: ImmutableMappable>(request: DataRequest, context: MapContext? = nil) -> Single<[T]> {
+        single(request: request, mapper: ObjectArrayMapper<T>(context: context))
     }
 
-    public func single<T: ImmutableMappable>(url: URLConvertible, method: HTTPMethod, parameters: Parameters = [:], encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, interceptor: RequestInterceptor? = nil, responseCacherBehavior: ResponseCacher.Behavior? = nil) -> Single<T> {
-        single(url: url, method: method, parameters: parameters, mapper: ObjectMapper<T>(), encoding: encoding, headers: headers, interceptor: interceptor, responseCacherBehavior: responseCacherBehavior)
+    public func single<T: ImmutableMappable>(url: URLConvertible, method: HTTPMethod, parameters: Parameters = [:], encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, interceptor: RequestInterceptor? = nil, responseCacherBehavior: ResponseCacher.Behavior? = nil, context: MapContext? = nil) -> Single<T> {
+        single(url: url, method: method, parameters: parameters, mapper: ObjectMapper<T>(context: context), encoding: encoding, headers: headers, interceptor: interceptor, responseCacherBehavior: responseCacherBehavior)
     }
 
-    public func single<T: ImmutableMappable>(url: URLConvertible, method: HTTPMethod, parameters: Parameters = [:], encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, interceptor: RequestInterceptor? = nil, responseCacherBehavior: ResponseCacher.Behavior? = nil) -> Single<[T]> {
-        single(url: url, method: method, parameters: parameters, mapper: ObjectArrayMapper<T>(), encoding: encoding, headers: headers, interceptor: interceptor, responseCacherBehavior: responseCacherBehavior)
+    public func single<T: ImmutableMappable>(url: URLConvertible, method: HTTPMethod, parameters: Parameters = [:], encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, interceptor: RequestInterceptor? = nil, responseCacherBehavior: ResponseCacher.Behavior? = nil, context: MapContext? = nil) -> Single<[T]> {
+        single(url: url, method: method, parameters: parameters, mapper: ObjectArrayMapper<T>(context: context), encoding: encoding, headers: headers, interceptor: interceptor, responseCacherBehavior: responseCacherBehavior)
     }
 
-    public func single<T: ImmutableMappable>(url: URLConvertible, method: HTTPMethod, parameters: Parameters = [:], encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, interceptor: RequestInterceptor? = nil, responseCacherBehavior: ResponseCacher.Behavior? = nil) -> Single<[String: T]> {
-        single(url: url, method: method, parameters: parameters, mapper: ObjectDictionaryMapper<T>(), encoding: encoding, headers: headers, interceptor: interceptor, responseCacherBehavior: responseCacherBehavior)
+    public func single<T: ImmutableMappable>(url: URLConvertible, method: HTTPMethod, parameters: Parameters = [:], encoding: ParameterEncoding = URLEncoding.default, headers: HTTPHeaders? = nil, interceptor: RequestInterceptor? = nil, responseCacherBehavior: ResponseCacher.Behavior? = nil, context: MapContext? = nil) -> Single<[String: T]> {
+        single(url: url, method: method, parameters: parameters, mapper: ObjectDictionaryMapper<T>(context: context), encoding: encoding, headers: headers, interceptor: interceptor, responseCacherBehavior: responseCacherBehavior)
     }
 
 }
@@ -30,37 +30,52 @@ extension NetworkManager {
 extension NetworkManager {
 
     class ObjectMapper<T: ImmutableMappable>: IApiMapper {
+        private let context: MapContext?
+
+        init(context: MapContext? = nil) {
+            self.context = context
+        }
 
         func map(statusCode: Int, data: Any?) throws -> T {
             guard let data = data else {
                 throw RequestError.invalidResponse(statusCode: statusCode, data: data)
             }
 
-            return try T(JSONObject: data)
+            return try T(JSONObject: data, context: context)
         }
 
     }
 
     class ObjectArrayMapper<T: ImmutableMappable>: IApiMapper {
+        private let context: MapContext?
+
+        init(context: MapContext? = nil) {
+            self.context = context
+        }
 
         func map(statusCode: Int, data: Any?) throws -> [T] {
             guard let data = data else {
                 throw RequestError.invalidResponse(statusCode: statusCode, data: data)
             }
 
-            return try Mapper<T>().mapArray(JSONObject: data)
+            return try Mapper<T>(context: context).mapArray(JSONObject: data)
         }
 
     }
 
     class ObjectDictionaryMapper<T: ImmutableMappable>: IApiMapper {
+        private let context: MapContext?
+
+        init(context: MapContext? = nil) {
+            self.context = context
+        }
 
         func map(statusCode: Int, data: Any?) throws -> [String: T] {
             guard let data = data else {
                 throw RequestError.invalidResponse(statusCode: statusCode, data: data)
             }
 
-            return try Mapper<T>().mapDictionary(JSONObject: data)
+            return try Mapper<T>(context: context).mapDictionary(JSONObject: data)
         }
 
     }
